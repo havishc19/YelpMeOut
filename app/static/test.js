37f516ea7659bbd0f5e8098816bd901cafced2d8
Havish Chennamraj <havishchennamraj@Havishs-MBP.attlocal.net>
2018-11-05 20:58:06 -0500

    Adding template files for d3 charts

--- app/static/test.js
+++ app/static/test.js
@@ -6,11 +6,6 @@
         color: '#000000'
       },
       {
-        language: 'Automobile',
-        value: 55,
-        color: '#00a2ee'
-      },
-      {
         language: 'Clothing',
         value: 68,
         color: '#fbcb39'
@@ -26,14 +21,19 @@
         color: '#65cedb'
       },
       {
-        language: 'Pet Services',
-        value: 34,
-        color: '#ff6e52'
+        language: 'Automobile',
+        value: 55,
+        color: '#00a2ee'
       },
       {
         language: 'Real Estate',
         value: 41,
         color: '#f9de3f'
+      },
+      {
+        language: 'Pet Services',
+        value: 34,
+        color: '#ff6e52'
       },
       {
         language: 'Dry Cleaning',
@@ -53,7 +53,7 @@
     ];
 
     const margin = 80;
-    const width = 1000 - 2 * margin;
+    const width = 1200 - 2 * margin;
     const height = 600 - 2 * margin;
 
     var svg = d3.select("#container").append("svg")
@@ -68,29 +68,30 @@
       .attr('transform', `translate(${margin}, ${margin})`);
 
     const xScale = d3.scaleBand()
-      .range([0, width])
+      .range([height, 0])
       .domain(sample.map((s) => s.language))
       .padding(0.4)
 
     const yScale = d3.scaleLinear()
-      .range([height, 0])
+      .range([0, width])
       .domain([0, 100]);
+
+    const yScale1 = d3.scaleLinear()
+      .range([0, width ])
+      .domain([0, 10]);
 
     // vertical grid lines
     // const makeXLines = () => d3.axisBottom()
     //   .scale(xScale)
 
-    const makeYLines = () => d3.axisLeft()
-      .scale(yScale)
+    const makeYLines = () => d3.axisBottom()
+      .scale(yScale1)
+
+
 
     chart.append('g')
-      .attr('transform', `translate(0, ${height})`)
-      .call(d3.axisBottom(xScale))
-      .selectAll("text")
-      .attr("transform", "rotate(-10)");
-
-    chart.append('g')
-      .call(d3.axisLeft(yScale));
+      .call(d3.axisBottom(yScale))
+      .attr('transform', `translate(0,${height})`);
 
     // vertical grid lines
     // chart.append('g')
@@ -107,6 +108,7 @@
         .tickSize(-width, 0, 0)
         .tickFormat('')
       )
+      .attr('transform', `translate(0,${height})`);
 
     const barGroups = chart.selectAll()
       .data(sample)
@@ -116,67 +118,67 @@
     barGroups
       .append('rect')
       .attr('class', 'bar')
-      .attr('x', (g) => xScale(g.language))
-      .attr('y', (g) => yScale(g.value))
-      .attr('height', (g) => height - yScale(g.value))
-      .attr('width', xScale.bandwidth())
-      .on('mouseenter', function (actual, i) {
-        d3.selectAll('.value')
-          .attr('opacity', 0)
+      .attr('y', (g) => xScale(g.language))
+      .attr('x', (g) => yScale(0.5))
+      .attr('width', (g) => yScale(g.value))
+      .attr('height', xScale.bandwidth())
+      // .on('mouseenter', function (actual, i) {
+      //   d3.selectAll('.value')
+      //     .attr('opacity', 0)
 
-        d3.select(this)
-          .transition()
-          .duration(300)
-          .attr('opacity', 0.6)
-          .attr('x', (a) => xScale(a.language) - 5)
-          .attr('width', xScale.bandwidth() + 10)
+      //   d3.select(this)
+      //     .transition()
+      //     .duration(300)
+      //     .attr('opacity', 0.6)
+      //     .attr('x', (a) => xScale(a.language) - 5)
+      //     .attr('height', xScale.bandwidth() + 10)
 
-        const y = yScale(actual.value)
+      //   const y = yScale(actual.value)
 
-        line = chart.append('line')
-          .attr('id', 'limit')
-          .attr('x1', 0)
-          .attr('y1', y)
-          .attr('x2', width)
-          .attr('y2', y)
+      //   line = chart.append('line')
+      //     .attr('id', 'limit')
+      //     .attr('x1', 0)
+      //     .attr('y1', y)
+      //     .attr('x2', width)
+      //     .attr('y2', y)
 
-        barGroups.append('text')
-          .attr('class', 'divergence')
-          .attr('x', (a) => xScale(a.language) + xScale.bandwidth() / 2)
-          .attr('y', (a) => yScale(a.value) + 30)
-          .attr('fill', 'white')
-          .attr('text-anchor', 'middle')
-          .text((a, idx) => {
-            const divergence = (a.value - actual.value).toFixed(1)
+      //   barGroups.append('text')
+      //     .attr('class', 'divergence')
+      //     .attr('y', (a) => xScale(a.language) + xScale.bandwidth() / 2)
+      //     .attr('x', (a) => yScale(a.value) + 30)
+      //     .attr('fill', 'white')
+      //     .attr('text-anchor', 'middle')
+      //     .text((a, idx) => {
+      //       const divergence = (a.value - actual.value).toFixed(1)
 
-            let text = ''
-            if (divergence > 0) text += '+'
-            text += `${divergence}%`
+      //       let text = ''
+      //       if (divergence > 0) text += '+'
+      //       text += `${divergence}%`
 
-            return idx !== i ? text : '';
-          })
+      //       return idx !== i ? text : '';
+      //     })
 
-      })
-      .on('mouseleave', function () {
-        d3.selectAll('.value')
-          .attr('opacity', 1)
+      // })
+      // .on('mouseleave', function () {
+      //   d3.selectAll('.value')
+      //     .attr('opacity', 1)
 
-        d3.select(this)
-          .transition()
-          .duration(300)
-          .attr('opacity', 1)
-          .attr('x', (a) => xScale(a.language))
-          .attr('width', xScale.bandwidth())
+      //   d3.select(this)
+      //     .transition()
+      //     .duration(300)
+      //     .attr('opacity', 1)
+      //     .attr('y', (a) => xScale(a.language))
+      //     .attr('height', xScale.bandwidth())
 
-        chart.selectAll('#limit').remove()
-        chart.selectAll('.divergence').remove()
-      })
+      //   chart.selectAll('#limit').remove()
+      //   chart.selectAll('.divergence').remove()
+      // })
 
     barGroups
       .append('text')
       .attr('class', 'value')
-      .attr('x', (a) => xScale(a.language) + xScale.bandwidth() / 2)
-      .attr('y', (a) => yScale(a.value) + 30)
+      .attr('y', (a) => xScale(a.language) + xScale.bandwidth() / 2)
+      .attr('x', (a) => yScale(a.value) + 30)
       .attr('text-anchor', 'middle')
       .text((a) => `${a.value}`)
 
@@ -187,21 +189,28 @@
       .attr('y', margin / 2.4)
       .attr('transform', 'rotate(-90)')
       .attr('text-anchor', 'middle')
-      .text('# Customers')
+      .text('Customer Influx')
 
     svg.append('text')
       .attr('class', 'label')
       .attr('x', width / 2 + margin)
       .attr('y', height + margin * 1.7)
       .attr('text-anchor', 'middle')
-      .text('Business')
+      .text('Business Category')
 
     svg.append('text')
       .attr('class', 'title')
       .attr('x', width / 2 + margin)
       .attr('y', 40)
       .attr('text-anchor', 'middle')
-      .text('Traffic Vs Time')
+      .text('Popularity of Businesses at various times')
+
+    chart.append('g')
+      .attr('transform', `translate(0,0)`)
+      .call(d3.axisLeft(xScale))
+      .attr('text-anchor', 'left')
+      .selectAll("text")
+      .attr("transform", `translate(20,0)`);
 
     // svg.append('text')
     //   .attr('class', 'source')
